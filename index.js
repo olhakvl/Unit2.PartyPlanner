cc9f5115abeaaf41f7139b102be2aae9bc33e715
olhakvl <olhakoval21@gmail.com>
2023-12-06 16:05:14 -0600

    updated get events function

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const COHORT = "2310-fsa-et-web-pt-sf-b-olha";
 const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/" + COHORT;
 
 const state = {
-    names: [],
-    dates: [],
-    times: [],
-    locations: [],
-    descriptions: []
+    events: []
   };
 
+
+// GET the list of events from the API to update state
+ 
 async function getEvents(){
     const events = await fetch(API + "/events");
     const jsonEvents = await events.json();
-    console.log(jsonEvents);
+    // console.log(jsonEvents);
+    state.events = jsonEvents.data;
+    // console.log(state.events);
+    return state.events;
 }
 
 getEvents();
